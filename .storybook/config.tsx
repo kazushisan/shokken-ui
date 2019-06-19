87e53c9696c1ddc8e0cad93838ccc25ff6fa0cfb
Kazushi Konosu <kazushi@konosu.me>
2019-06-19 12:38:16 +0900

    init styled components

---
+++ .storybook/config.tsx
@@ -0,0 +1,21 @@
+import { addDecorator, configure } from '@storybook/react'
+import React from 'react'
+import GlobalStyle from '../GlobalStyle'
+
+function withGlobalStyles(storyFn) {
+  return (
+    <React.Fragment>
+      <GlobalStyle />
+      {storyFn()}
+    </React.Fragment>
+  )
+}
+
+addDecorator(withGlobalStyles)
+
+const req = require.context('../stories', true, /\.tsx$/)
+function loadStories() {
+  req.keys().forEach(filename => req(filename))
+}
+
+configure(loadStories, module)

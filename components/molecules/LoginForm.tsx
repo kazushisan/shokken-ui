45522d93cb0c94e1dd9440c7849de588e701272d
SakiMuraoka <murasaki9322@gmail.com>
2019-06-19 13:53:09 +0900

    init loginform

---
+++ components/molecules/LoginForm.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Button from '../atoms/Button'
+import Input from '../atoms/Input'
+
+class LoginForm extends React.Component<> {
+  render() {
+    ;<div>Login Component</div>
+  }
+}

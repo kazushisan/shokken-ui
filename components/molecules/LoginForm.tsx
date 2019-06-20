aead99fecd4c10a1385fa7e43a5acfd3817cd8f6
SakiMuraoka <murasaki9322@gmail.com>
2019-06-20 21:00:10 +0900

    first_commit

--- components/molecules/LoginForm.tsx
+++ components/molecules/LoginForm.tsx
@@ -2,8 +2,39 @@ import React from 'react'
 import Button from '../atoms/Button'
 import Input from '../atoms/Input'
 
-class LoginForm extends React.Component<> {
+interface Props {
+  form1: string
+  form2: string
+  label: string
+}
+
+class LoginForm extends React.Component<Props> {
+  constructor() {
+    super()
+    this.state = { name: 'name', pass: 'pass' }
+  }
+
+  onClick() {
+    this.setState(state => ({
+      name: 'NAME',//nameの値に変更する
+      pass: 'PASS'//passの値に変更する
+    }));
+  }
+
   render() {
-    ;<div>Login Component</div>
+    return (
+      <div>
+        <p>Login Window</p>
+        <Input id="name" color="primary" placeholder={this.props.form1} />
+        <Input id="pass" color="primary" placeholder={this.props.form2} />
+        <Button color="primary" onClick={this.onClick}>
+          {this.props.label}
+        </Button>
+        {this.state.name}
+        {this.state.pass}
+      </div>
+    )
   }
 }
+
+export default LoginForm

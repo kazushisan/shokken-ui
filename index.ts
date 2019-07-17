b726ca2b1a0a2e49a667de4211a24ff8e95ab2b7
Kazushi Konosu <kazushi@konosu.me>
2019-07-17 13:22:51 +0900

    add index

---
+++ index.ts
@@ -0,0 +1,15 @@
+// atoms
+import Button from './components/atoms/Button'
+import Input from './components/atoms/Input'
+
+// molecules
+import AdminOrderCard from './components/molecules/AdminOrderCard'
+import LoginForm from './components/molecules/LoginForm'
+import UserOrderCard from './components/molecules/UserOrderCard'
+
+// organisms
+import AdminOrderList from './components/organisms/AdminOrderList'
+
+export const atoms = { Button, Input }
+export const molecules = { AdminOrderCard, LoginForm, UserOrderCard }
+export const organisms = { AdminOrderList }

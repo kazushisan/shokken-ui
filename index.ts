2c4eae92c2308fab5d93d75b3eb179cf2231705f
SakiMuraoka <murasaki9322@gmail.com>
2019-07-17 14:51:19 +0900

    index.tsに追加

--- index.ts
+++ index.ts
@@ -5,6 +5,8 @@ import Input from './components/atoms/Input'
 // molecules
 import AdminOrderCard from './components/molecules/AdminOrderCard'
 import LoginForm from './components/molecules/LoginForm'
+import AdminProductCard from './components/molecules/AdminProductCard'
+
 
 // organisms
 import AdminOrderList from './components/organisms/AdminOrderList'
@@ -17,7 +19,8 @@ export const atoms: {
 export const molecules: {
   AdminOrderCard: typeof AdminOrderCard
   LoginForm: typeof LoginForm
-} = { AdminOrderCard, LoginForm }
+  AdminProductCard: typeof AdminProductCard
+} = { AdminOrderCard, LoginForm, AdminProductCard }
 
 export const organisms: {
   AdminOrderList: typeof AdminOrderList
